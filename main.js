f3e17a0469ef248a610e192722d21f1c8f8a5d6b
daniyall_sidikii <160391502+daniallsidikii@users.noreply.github.com>
2024-03-23 17:20:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// MINI CALCULATOR:
+let prompts = require("prompt-sync")();
+let n1 = parseInt(prompts("enter first number: "));
+let n2 = parseInt(prompts("enter second number: "));
+let operator = prompts("enter operator (+,-,/,*): ");
+if (operator === "+") {
+    console.log("the sum is: ", n1 + n2);
+}
+else if (operator === "-") {
+    console.log("the substraction is: ", n1 - n2);
+}
+else if (operator === "*") {
+    console.log("the multiplication is: ", n1 * n2);
+}
+else {
+    console.log("the division is is: ", n1 / n2);
+}
